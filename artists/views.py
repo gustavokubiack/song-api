489cc41811d99a6035dc7af5d840eecfac90e0dd
gustavokubiack <guukubiack@gmail.com>
2024-09-18 23:11:31 -0300

    feat: include authentication on all api endpoints

--- artists/views.py
+++ artists/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from artists.models import Artist
 from artists.serializers import ArtistModelSerializer
 
 
 class ArtistCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Artist.objects.all()
     serializer_class = ArtistModelSerializer
 
 
 class ArtistRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Artist.objects.all()
     serializer_class = ArtistModelSerializer
--- songs/views.py
+++ songs/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from songs.models import Song
 from songs.serializers import SongModelSerializer
 
 
 class SongCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Song.objects.all()
     serializer_class = SongModelSerializer
 
 
 class SongRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Song.objects.all()
     serializer_class = SongModelSerializer
