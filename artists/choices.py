1b3221a03e935a3649044c19d4319919c8543adf
gustavokubiack <guukubiack@gmail.com>
2024-09-18 21:32:10 -0300

    fix: nationality choices

--- artists/choices.py
+++ artists/choices.py
@@ -2,5 +2,5 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 class NationalityTypeChoices(models.TextChoices):
-    USA = 'USA', _('Estados Unidos')
-    BRAZIL = 'BRAZIL', _('Brasil')
+    USA = 'USA', _('United States of America')
+    BRAZIL = 'BR', _('Brazil')
